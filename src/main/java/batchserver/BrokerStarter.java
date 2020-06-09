fba05b9ba02b41d3193000f04fd7e64cd2d01fda
sujil02 <sujil.shah@gmail.com>
2020-06-09 05:24:31 -0400

    Local changes
    Signed-off-by: sujil02 <sujil.shah@gmail.com>

--- src/main/java/batchserver/BrokerStarter.java
+++ src/main/java/batchserver/BrokerStarter.java
@@ -4,7 +4,7 @@ import org.apache.activemq.broker.BrokerService;
 
 public class BrokerStarter {
 
-	protected static String cmd_url = "tcp://activemq:61616";
+	protected static String cmd_url = "tcp://localhost:61616";
 
 	public static void initCmdBroker() throws Exception {
 		final BrokerService broker = new BrokerService();
